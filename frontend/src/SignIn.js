98ef5a3089f7c15e9c712c4645f379b48a9b3e73
Nowado <spam@jakubkrolikowski.pl>
2022-10-29 09:27:23 +0200

    removed unused clutter

--- frontend/src/SignIn.js
+++ frontend/src/SignIn.js
@@ -11,8 +11,6 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import axios from 'axios';
-import { Navigate, Outlet } from "react-router-dom";
-import { useLocation } from "react-router";
 
 import { signInToken } from './auth';
 
@@ -20,7 +18,6 @@ const theme = createTheme();
 
 export default function SignIn() {
   const signIn = signInToken;
-  const location = useLocation();
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
